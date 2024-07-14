81a0ace61ed08a0d25c39532b5bc4b947a1ede36
Diaa <dia.saada@gmail.com>
2024-07-14 19:13:53 +0400

    Adding more validation

--- src/main/java/com/conference/app/booking/BookDTO.java
+++ src/main/java/com/conference/app/booking/BookDTO.java
@@ -1,17 +1,25 @@
 package com.conference.app.booking;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 
 public class BookDTO {
 
 
     @NotNull(message = "startAt is required")
+    @Size(min = 4, max = 4, message = "startAt must be 4 characters")
     public String startAt;
+
     @NotNull(message = "endAt is required")
+    @Size(min = 4, max = 4, message = "startAt must be 4 characters")
     public String endAt;
+
     @NotNull(message = "attendees is required")
+    @Min(value = 11, message = "attendees should not be less than 1")
+    @Max(value = 9999, message = "attendees should not be greater than 9999")
     public int attendees;
+
     @NotNull(message = "bookingDate is required")
+    @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$", message = "Date must be in the format yyyy-MM-dd")
     public String bookingDate;
 
     public BookDTO(String startAt, String endAt, int attendees, String bookingDate) {
